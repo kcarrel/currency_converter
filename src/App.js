6e23417d16222ed0195923d67547da5b78dce90a
Katie Carrel <katiecarrel@comcast.net>
2020-10-20 21:49:01 -0700

    basic form styling

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 
 const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
   paper: {
+      width: 500,
+      height: 500,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column',
       backgroundColor: "#eee",
-      marginLeft: "30%",
-      marginRight: "30%",
-      height: "100%"
+  },
+  form: {
+    width: '100%',
   },
   textField: {
       backgroundColor: "#fff"
@@ -34,45 +43,57 @@ function ConvertForm() {
 
 
   return (
-    <div className="convert_form">
-      <Paper className={classes.paper}>
-        <FormControl>
-          <TextField
-            name='amount'
-            label="Amount to Convert"
-            type="number"
-            onChange={e => setAmount(e.target.value)}
-            value={amount}
-          />
-        </FormControl>
-        <FormControl>
-          <InputLabel id="native_currency">Native Currency</InputLabel>
-          <Select
-            name='native_currency'
-            labelId="native_currency"
-            type="text"
-            onChange={e => setNativeCurrency(e.target.value)}
-            value={native_currency}
-          >
-            <MenuItem >GDP</MenuItem>
-          </Select>
-        </FormControl>
-        <FormControl>
-          <InputLabel id="foreign_currency">Foreign Currency</InputLabel>
-          <Select
-            name='foreign_currency'
-            labelId="foreign_currency"
-            type="text"
-            onChange={e => setForeignCurrency(e.target.value)}
-            value={foreign_currency}
-          >
-            <MenuItem >GDP</MenuItem>
-          </Select>
-        </FormControl>
-        <Button onClick={convertAmount} color="primary" variant="contained">
-          Convert
-        </Button>
-      </Paper>
+    <div className={classes.root}>
+      <div className="currency_form">
+        <Paper className={classes.paper}>
+          <form>
+            <TextField
+              margin="normal"
+              name='amount'
+              required
+              fullWidth
+              label="Amount to Convert"
+              type="number"
+              onChange={e => setAmount(e.target.value)}
+              value={amount}
+            />
+            <FormControl fullWidth margin="normal">
+              <InputLabel id="native_currency">Native Currency</InputLabel>
+              <Select
+                required
+                fullWidth
+                name='native_currency'
+                labelId="native_currency"
+                type="text"
+                onChange={e => setNativeCurrency(e.target.value)}
+                value={native_currency}
+              >
+                <MenuItem >GDP</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl fullWidth margin="normal">
+              <InputLabel id="foreign_currency">Foreign Currency</InputLabel>
+              <Select
+                name='foreign_currency'
+                labelId="foreign_currency"
+                type="text"
+                onChange={e => setForeignCurrency(e.target.value)}
+                value={foreign_currency}
+              >
+                <MenuItem >GDP</MenuItem>
+              </Select>
+            </FormControl>
+            <Button 
+              fullWidth 
+              margin="normal"
+              onClick={convertAmount} 
+              color="primary" 
+              variant="contained">
+              Convert
+            </Button>
+          </form>
+        </Paper>
+      </div>
     </div>
   );
 };
